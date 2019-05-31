98ffc84a64e0ff5910b889e0c7a6d6a450f2222e
Ivo <ilegzdins@yahoo.com>
2019-05-31 11:37:15 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there again again' });
+  res.send({ hi: 'there again again, second test' });
 });
 
 const PORT = process.env.PORT || 5000;
